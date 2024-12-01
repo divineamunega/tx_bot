8c76c51139e7f2eacb296ed9e3cf4a81dddcfb25
divineamunega <divineamunega@gmail.com>
2024-12-01 22:12:49 +0100

    feat added mongoose 🚀

--- src/bot/start.ts
+++ src/bot/start.ts
@@ -1,12 +1,17 @@
 import { Telegraf, Markup } from "telegraf";
+const X_URL = "https://6fb6-102-89-22-218.ngrok-free.app/";
 
 const startBot = function (bot: Telegraf) {
 	bot.start((ctx) => {
-		// const telegramId = ctx.chat.id;
+		const telegramId = ctx.chat.id;
+
+		// Save this in the database as user_telegram_start_code
+
+		// Maybe I can hash it
 		ctx.reply(
 			"Click below to connect your X account:",
 			Markup.inlineKeyboard([
-				Markup.button.url("Connect X", "http://localhost/auth/x"),
+				Markup.button.url("Connect X", `${X_URL}auth/x?${telegramId}`),
 			])
 		);
 	});
--- src/x_service/routes/AuthRoutes.ts
+++ src/x_service/routes/AuthRoutes.ts
@@ -5,5 +5,13 @@ import passport from "../passport";
 const router = Router();
 
 router.get("/x-auth-callback", authenticateX);
-router.get("/x", passport.authenticate("twitter", { failureRedirect: "/x" }));
+router.get(
+	"/x",
+	() => {
+		/// Compare and search for the users telegram id in the database
+		// If not there disallow
+		// If there allow
+	},
+	passport.authenticate("twitter", { failureRedirect: "/x" })
+);
 export default router;
