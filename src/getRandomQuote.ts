403744156f5e36c5fb6f9687be1c42d1c539a06b
divineamunega <divineamunega@gmail.com>
2025-01-04 03:24:07 +0100

    feat: quote restart

--- src/getRandomQuote.ts
+++ src/getRandomQuote.ts
@@ -1,15 +1,30 @@
+import delay from "./utils/delay";
+
 const BASE_URL = "https://zenquotes.io/api/random/[your_key]";
 
-const getQuote = async function () {
-	try {
-		const res = await fetch(BASE_URL);
-		if (!res.ok) throw new Error("An Error occured while fetching the qoutes");
+const getQuote = async function (retry = 3) {
+	let numRetry = 0;
 
-		const data = await res.json();
-		return data[0];
-	} catch (err: any) {
-		console.log(err);
-		throw new Error(err.message);
+	while (numRetry < retry) {
+		numRetry++;
+		try {
+			const res = await fetch(BASE_URL);
+			if (!res.ok)
+				throw new Error("An Error occured while fetching the qoutes");
+
+			const data = await res.json();
+			return data[0];
+		} catch (err: any) {
+			if (numRetry <= retry) {
+				console.log(`${err.message} ${numRetry}/${retry}`);
+				await delay(3);
+			}
+
+			if (numRetry === retry) {
+				console.log("COULD NOT GET QUOTES");
+				throw new Error(err);
+			}
+		}
 	}
 };
 
